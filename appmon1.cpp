3bde5f619b70fef0cd76068749a899ab72a38c54
Calum <calumb@gmail.com>
2013-04-02 12:18:55 -0400

    appmon close. fully functioning. starting to add timing

--- appmon1.cpp
+++ appmon1.cpp
@@ -7,8 +7,8 @@ server.log(format("Running at %.2f V", voltage));
 //////////setup vars///////////////////////////////////////
 local readTime = 100;   //time in ms
 local arraySize = 100; //readings per sample
-local lowThresh = 3;
-local vibratioThreshold = 50;   //check vibration minimum
+local lowThresh = 0;
+local vibratioThresh = 70;   //check vibration minimum
 local wakeVibratio = 50;    //wake from sleep vibration
 //sleep
 local delayMS = 10000;  //delay time in MS
@@ -21,6 +21,8 @@ local delayMS = 10000;  //delay time in MS
  local zDelta = 0;  //calced
  
  local zValues = [arraySize];
+ local zVibs = [10];
+ local runKeeper = [10];
  local index = 0;   //handy
  
  local newArray = false;
@@ -30,10 +32,19 @@ local delayMS = 10000;  //delay time in MS
  ///////////////////////
  
 function checkVibs() {
-    imp.wakeup(5.0, checkVibs);
+    imp.wakeup(10.0, checkVibs);
     
-    server.log(takeVibratio());
-   
+    local checkedVib = takeVibratio();
+    
+    if(checkedVib > wakeVibratio){
+        server.log("woken");
+        if(runDetect()){
+            agent.send("run",hardware.millis());
+            server.log("going to sleep");
+            //imp.onidle(function() { server.sleepfor(30.0); } );
+            server.sleepfor(30.0);
+        }
+    }
  
     
 }
@@ -62,7 +73,7 @@ function takeVibratio(){
     
     if(newData){
         server.log("newData ->" + hardware.millis());
-        xVibratio = hardware.pin1.read();
+        
         local lowDelta = 0;
         
         foreach(val in zValues){
@@ -73,11 +84,11 @@ function takeVibratio(){
         lowDelta--;
         
 
-        server.log(lowDelta);
-        server.log(arraySize);
+        // server.log(lowDelta);
+        //server.log(arraySize);
         local sizef = arraySize.tofloat();
         xVibratio = ((arraySize - lowDelta) / sizef) * 100;
-        server.log(xVibratio);
+        server.log("vibratio:" + xVibratio);
         
         newData = 0;
         zValues.clear();
@@ -86,7 +97,30 @@ function takeVibratio(){
     return xVibratio;
 }
 
-
+function runDetect(){
+    local boolRunning = 0;
+    
+    for(local i = 0;i < 10; i++){
+        zVibs.insert(i,takeVibratio());
+        if(zVibs[i] >= vibratioThresh){
+            runKeeper.insert(i,1);
+        }else runKeeper.insert(i,0);
+    }
+    
+    local vib1count = 0;
+    local vib0count = 0;
+    foreach(val in runKeeper){
+        if(val == 1){
+            vib1count++;
+        }else vib0count++;
+    }
+    if((vib1count - vib0count) >= 2){
+        server.log("run has been detected");
+        boolRunning = true;
+    }else server.log("no run detected in runDetect()")
+    
+    return boolRunning;
+}
 
 //starters 
 server.log("Accelerometer Started");
